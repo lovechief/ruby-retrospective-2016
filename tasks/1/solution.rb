c0e6a0defddf4456b0b6757990d9fb1e239577e3
Delyan Lafchiev <Delyan Lafchiev>
2017-01-16 16:49:19 +0200

    Improve Problem 1

--- tasks/1/solution.rb
+++ tasks/1/solution.rb
@@ -1,63 +1,23 @@
 def convert_between_temperature_units(degrees, src_units, target_units)
-	case src_units
-	when 'K'
-		case target_units
-		when 'K'
-			degrees
-		when 'C'
-			degrees - 273.15
-		when 'F'
-			(9 * degrees) / 5.0 - 459.67
-		end
-	when 'C'
-		case target_units
-		when 'K'
-			degrees + 273.15
-		when 'C'
-			degrees
-		when 'F'
-			(9 * degrees) / 5.0 + 32
-		end
-	when 'F'
-		case target_units
-		when 'K'
-			(degrees + 459.67) * 5.0 / 9
-		when 'C'
-			(degrees - 32) * 5.0 / 9
-		when 'F'
-			degrees
-		end
-	end
+    coefficient = { 'C' => 1.8, 'F' => 1, 'K' => 1.8 }
+    constant = { 'C' => 32,  'F' => 0, 'K' => -459.67 }
+	(degrees * coefficient[src_units] + constant[src_units] - constant[target_units]) / coefficient[target_units]
 end
 
+SUBSTANCES = {
+    'water'   => {melting_point: 0,     boiling_point: 100  },
+    'ethanol' => {melting_point: -114,  boiling_point: 78.37},
+    'gold'    => {melting_point: 1_064, boiling_point: 2_700},
+    'silver'  => {melting_point: 961.8, boiling_point: 2_162},
+    'copper'  => {melting_point: 1_085, boiling_point: 2_567}
+}
+
 def melting_point_of_substance(substance, units)
-	case substance
-	when 'water'
-		convert_between_temperature_units(0, 'C', units)
-	when 'ethanol'
-		convert_between_temperature_units(-114, 'C', units)
-	when 'gold'
-		convert_between_temperature_units(1_064, 'C', units)
-	when 'silver'
-		convert_between_temperature_units(961.8, 'C', units)
-	when 'copper'
-		convert_between_temperature_units(1_085, 'C', units)
-	end
+    convert_between_temperature_units(SUBSTANCES[substance][:melting_point], 'C', units)
 end
 
 def boiling_point_of_substance(substance, units)
-	case substance
-	when 'water'
-		convert_between_temperature_units(100, 'C', units)
-	when 'ethanol'
-		convert_between_temperature_units(78.37, 'C', units)
-	when 'gold'
-		convert_between_temperature_units(2_700, 'C', units)
-	when 'silver'
-		convert_between_temperature_units(2_162, 'C', units)
-	when 'copper'
-		convert_between_temperature_units(2_567, 'C', units)
-	end
+    convert_between_temperature_units(SUBSTANCES[substance][:boiling_point], 'C', units)
 end
 
 p convert_between_temperature_units(0, 'C', 'K')
